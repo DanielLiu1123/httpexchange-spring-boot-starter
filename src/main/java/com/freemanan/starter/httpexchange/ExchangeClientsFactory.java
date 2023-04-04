43eb72c4b95fd6c6d52f09e3ab86fd89f45cadd9
Freeman Liu <llw599502537@gmail.com>
2023-04-04 22:42:38 +0800

    optimize ExchangeClientsFactory

--- src/main/java/com/freemanan/starter/httpexchange/ExchangeClientsFactory.java
+++ src/main/java/com/freemanan/starter/httpexchange/ExchangeClientsFactory.java
@@ -1,7 +1,10 @@
 package com.freemanan.starter.httpexchange;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.Map;
 import java.util.function.Supplier;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.util.Assert;
@@ -33,19 +36,8 @@ public class ExchangeClientsFactory {
      */
     @SuppressWarnings("unchecked")
     public <T> T create() {
-        HttpServiceProxyFactory.Builder builder = beanFactory
-                .getBeanProvider(HttpServiceProxyFactory.Builder.class)
-                .getIfUnique();
-        // look up from beanFactory
-        // 1. look up HttpServiceProxyFactory.Builder first
-        if (builder != null) {
-            return (T) builder
-                    // support url placeholder '${}'
-                    .embeddedValueResolver(beanFactory.getBean(Environment.class)::resolvePlaceholders)
-                    .build()
-                    .createClient(type);
-        }
-        // 2. look up HttpServiceProxyFactory
+        // look up from bean factory
+        // 1. look up HttpServiceProxyFactory first
         HttpServiceProxyFactory factory =
                 beanFactory.getBeanProvider(HttpServiceProxyFactory.class).getIfUnique();
         if (factory != null) {
@@ -54,27 +46,63 @@ public class ExchangeClientsFactory {
             return (T) factory.createClient(type);
         }
 
+        // 2. look up HttpServiceProxyFactory.Builder
+        // NOTE: HttpServiceProxyFactory.Builder is a prototype bean, we need to cache it
+        // see https://github.com/spring-projects/spring-boot/issues/31337
+        // see https://github.com/spring-projects/spring-framework/issues/29296
+        HttpServiceProxyFactory.Builder builder =
+                Holder.getOrSupply(beanFactory, HttpServiceProxyFactory.Builder.class, () -> beanFactory
+                        .getBeanProvider(HttpServiceProxyFactory.Builder.class)
+                        .getIfUnique());
+        if (builder != null) {
+            return (T) builder
+                    // support url placeholder '${}'
+                    .embeddedValueResolver(beanFactory.getBean(Environment.class)::resolvePlaceholders)
+                    .build()
+                    .createClient(type);
+        }
+
         // look up from holder, don't add to beanFactory
         HttpServiceProxyFactory cachedFactory =
-                Holder.getOrSupply(() -> HttpServiceProxyFactory.builder(WebClientAdapter.forClient(beanFactory
-                                .getBeanProvider(WebClient.Builder.class)
-                                .getIfUnique(WebClient::builder)
-                                .build()))
+                Holder.getOrSupply(beanFactory, HttpServiceProxyFactory.class, () -> HttpServiceProxyFactory.builder(
+                                WebClientAdapter.forClient(getWebClient()))
                         // support url placeholder '${}'
                         .embeddedValueResolver(beanFactory.getBean(Environment.class)::resolvePlaceholders)
                         .build());
+        Assert.notNull(cachedFactory, "cachedFactory must not be null");
         return (T) cachedFactory.createClient(type);
     }
 
-    private static class Holder {
-        private static final AtomicReference<HttpServiceProxyFactory> INSTANCE = new AtomicReference<>();
+    private WebClient getWebClient() {
+        // lookup existing WebClient bean
+        WebClient webClientBean = beanFactory.getBeanProvider(WebClient.class).getIfUnique();
+        if (webClientBean != null) {
+            return webClientBean;
+        }
+        // lookup existing WebClient.Builder bean, create WebClient bean
+        return beanFactory
+                .getBeanProvider(WebClient.Builder.class)
+                .getIfUnique(WebClient::builder)
+                .build();
+    }
 
-        public static HttpServiceProxyFactory getOrSupply(Supplier<HttpServiceProxyFactory> supplier) {
-            if (INSTANCE.get() != null) {
-                return INSTANCE.get();
+    private static class Holder {
+        private static final Object NULL = new Object();
+        /**
+         * Use BeanFactory as the first-level cache, because in the tests, the Holder will be reused, causing the cache has unexpected behavior.
+         */
+        private static final Map<BeanFactory, Map<Class<?>, Object>> cache = new IdentityHashMap<>();
+
+        public static <T> T getOrSupply(BeanFactory beanFactory, Class<T> clazz, Supplier<T> supplier) {
+            Object cachedValue =
+                    cache.computeIfAbsent(beanFactory, k -> new HashMap<>()).get(clazz);
+            if (cachedValue != null) {
+                return cachedValue != NULL ? clazz.cast(cachedValue) : null;
             }
-            INSTANCE.compareAndSet(null, supplier.get());
-            return INSTANCE.get();
+            T value = supplier.get();
+            Object cacheValue = value != null ? value : NULL;
+            cache.computeIfAbsent(beanFactory, k -> new HashMap<>()).put(clazz, cacheValue);
+            return value;
         }
     }
 }
