4672f9979523e09084e38cd261f2eacfd959ab3e
Freeman Liu <llw599502537@gmail.com>
2023-04-15 23:18:44 +0800

    fix unit test

--- src/test/java/com/freemanan/starter/httpexchange/BaseUrlTests.java
+++ src/test/java/com/freemanan/starter/httpexchange/BaseUrlTests.java
@@ -1,57 +1,71 @@
 package com.freemanan.starter.httpexchange;
 
+import static com.freemanan.starter.Dependencies.springBootVersion;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
-import javax.net.ssl.SSLHandshakeException;
+import com.freemanan.cr.core.anno.Action;
+import com.freemanan.cr.core.anno.ClasspathReplacer;
+import com.freemanan.starter.PortFinder;
+import java.net.SocketException;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 import org.springframework.web.service.annotation.GetExchange;
 
 /**
  * @author Freeman
  */
+@ClasspathReplacer(@Action("org.springframework.boot:spring-boot-starter-webflux:" + springBootVersion))
 public class BaseUrlTests {
 
     @Test
     void testDefaultBaseUrl() {
-        var ctx = new SpringApplicationBuilder(BaseUrlConfig.class)
-                .web(WebApplicationType.NONE)
-                .properties(HttpClientsProperties.PREFIX + ".base-url=https://httpbin.org")
+        int port = PortFinder.availablePort();
+        var ctx = new SpringApplicationBuilder(BaseUrlController.class)
+                .properties("server.port=" + port)
+                .properties(HttpClientsProperties.PREFIX + ".base-url=localhost:" + port)
                 .run();
         BaseUrlApi api = ctx.getBean(BaseUrlApi.class);
 
-        assertThatCode(() -> api.delay(1)).doesNotThrowAnyException();
+        assertThatCode(() -> api.delay(10)).doesNotThrowAnyException();
 
         ctx.close();
     }
 
     @Test
     void testNoBaseUrl() {
-        var ctx = new SpringApplicationBuilder(BaseUrlConfig.class)
-                .web(WebApplicationType.NONE)
+        int port = PortFinder.availablePort();
+        var ctx = new SpringApplicationBuilder(BaseUrlController.class)
+                .properties("server.port=" + port)
                 .run();
         BaseUrlApi api = ctx.getBean(BaseUrlApi.class);
 
-        assertThatCode(() -> api.delay(1)).isInstanceOf(IllegalArgumentException.class);
+        assertThatCode(() -> api.delay(10))
+                .isInstanceOf(WebClientRequestException.class)
+                .hasMessageContaining("Connection refused:");
 
         ctx.close();
     }
 
     @Test
     void testBaseUrl_whenClientHasBaseUrl_thenOverrideDefaultBaseUrl() {
-        var ctx = new SpringApplicationBuilder(BaseUrlConfig.class)
-                .web(WebApplicationType.NONE)
-                .properties(HttpClientsProperties.PREFIX + ".base-url=https://httpbin.org")
+        int port = PortFinder.availablePort();
+        var ctx = new SpringApplicationBuilder(BaseUrlController.class)
+                .properties("server.port=" + port)
+                .properties(HttpClientsProperties.PREFIX + ".base-url=localhost:" + port)
                 .properties(HttpClientsProperties.PREFIX + ".clients[0].name=BaseUrlApi")
-                .properties(HttpClientsProperties.PREFIX + ".clients[0].base-url=https://httpbinxxxxxxxx.org")
+                .properties(HttpClientsProperties.PREFIX + ".clients[0].base-url=localhost:" + (port + 1))
                 .run();
         BaseUrlApi api = ctx.getBean(BaseUrlApi.class);
 
-        assertThatCode(() -> api.delay(1)).hasCauseInstanceOf(SSLHandshakeException.class);
+        assertThatCode(() -> api.delay(10))
+                .hasRootCauseInstanceOf(SocketException.class)
+                .hasMessageContaining("Invalid argument");
 
         ctx.close();
     }
@@ -59,7 +73,18 @@ public class BaseUrlTests {
     @Configuration(proxyBeanMethods = false)
     @EnableAutoConfiguration
     @EnableExchangeClients(clients = BaseUrlApi.class)
-    static class BaseUrlConfig {}
+    @RestController
+    static class BaseUrlController implements BaseUrlApi {
+        @Override
+        @GetMapping("/delay/{delay}")
+        public void delay(int delay) {
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     interface BaseUrlApi {
 
