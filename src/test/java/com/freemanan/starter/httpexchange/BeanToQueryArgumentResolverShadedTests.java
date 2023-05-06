d750d20cf9b5094ede2cae04d94be926e7c421f2
Freeman Liu <llw599502537@gmail.com>
2023-05-06 22:40:22 +0800

    not import *

--- src/test/java/com/freemanan/starter/httpexchange/BeanToQueryArgumentResolverShadedTests.java
+++ src/test/java/com/freemanan/starter/httpexchange/BeanToQueryArgumentResolverShadedTests.java
@@ -14,7 +14,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * {@link BeanToQueryArgumentResolver} tester.
