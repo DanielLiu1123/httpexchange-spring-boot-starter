bfb0734bd8537776f611f9022c0166b0525d3f60
Freeman Liu <llw599502537@gmail.com>
2023-06-07 21:53:48 +0800

    use jakarta namespace Nullable

--- src/main/java/com/freemanan/starter/httpexchange/UrlPlaceholderStringValueResolver.java
+++ src/main/java/com/freemanan/starter/httpexchange/UrlPlaceholderStringValueResolver.java
@@ -1,10 +1,10 @@
 package com.freemanan.starter.httpexchange;
 
+import jakarta.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.core.env.Environment;
-import org.springframework.lang.Nullable;
 import org.springframework.util.StringValueResolver;
 
 /**
