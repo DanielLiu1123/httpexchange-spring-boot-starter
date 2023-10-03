383b5f0c4a64b58f58df894b1e72fbde1dc4ecb5
Freeman Liu <llw599502537@gmail.com>
2023-10-03 22:21:16 +0800

    remove slf4j

--- examples/quick-start/src/main/java/com/example/server/QuickStartApp.java
+++ examples/quick-start/src/main/java/com/example/server/QuickStartApp.java
@@ -1,11 +1,9 @@
 package com.example.server;
 
 import com.freemanan.starter.httpexchange.EnableExchangeClients;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 @EnableExchangeClients("com.example.api")
 public class QuickStartApp {
