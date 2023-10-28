9265cdbfcca575f222882422e5ecaf58e5025004
Freeman Liu <llw599502537@gmail.com>
2023-10-28 19:31:46 +0800

    optimize quick start

--- examples/quick-start/src/main/java/com/example/server/QuickStartApp.java
+++ examples/quick-start/src/main/java/com/example/server/QuickStartApp.java
@@ -1,11 +1,9 @@
 package com.example.server;
 
-import com.freemanan.starter.httpexchange.EnableExchangeClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableExchangeClients("com.example.api")
 public class QuickStartApp {
     public static void main(String[] args) {
         SpringApplication.run(QuickStartApp.class, args);
