831208b5ec96d77ae67e94402cb48b3d48677843
Freeman Liu <llw599502537@gmail.com>
2023-11-25 14:58:08 +0800

    3.2.0-RC2

--- examples/quick-start/src/main/java/com/example/server/QuickStartApp.java
+++ examples/quick-start/src/main/java/com/example/server/QuickStartApp.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class QuickStartApp extends UserApiBase {
+
     public static void main(String[] args) {
         SpringApplication.run(QuickStartApp.class, args);
     }
