99540522d4c47cdf231208550fea105ddfc646dd
Freeman Liu <llw599502537@gmail.com>
2024-01-15 23:40:23 +0800

    Not apply java-library for all projects

--- examples/native-image/src/main/java/com/example/NativeImageApp.java
+++ examples/native-image/src/main/java/com/example/NativeImageApp.java
@@ -1,23 +1,27 @@
 package com.example;
 
+import io.github.danielliu1123.httpexchange.EnableExchangeClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
+import java.util.List;
 
 @SpringBootApplication
-// @EnableExchangeClients
-// @ImportRuntimeHints(HttpExchangeHints.class)
+@EnableExchangeClients
 public class NativeImageApp {
 
     public static void main(String[] args) {
         SpringApplication.run(NativeImageApp.class, args);
     }
 
-    //    @HttpExchange("https://my-json-server.typicode.com")
-    //    interface PostApi {
-    //        record Post(Integer id, String title) {
-    //        }
-    //
-    //        @GetExchange("/typicode/demo/posts")
-    //        List<Post> list();
-    //    }
+    @HttpExchange("https://my-json-server.typicode.com")
+    interface PostApi {
+        record Post(Integer id, String title) {
+        }
+
+        @GetExchange("/typicode/demo/posts")
+        List<Post> list();
+    }
 }
