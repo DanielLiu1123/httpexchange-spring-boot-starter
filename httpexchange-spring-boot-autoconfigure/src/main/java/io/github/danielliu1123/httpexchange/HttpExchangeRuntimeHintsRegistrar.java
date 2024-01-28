a4fd6c6b3eae166fd52af0685bcc947adcc938fa
Freeman Liu <llw599502537@gmail.com>
2024-01-29 02:06:01 +0800

    Add listFactory method

--- httpexchange-spring-boot-autoconfigure/src/main/java/io/github/danielliu1123/httpexchange/HttpExchangeRuntimeHintsRegistrar.java
+++ httpexchange-spring-boot-autoconfigure/src/main/java/io/github/danielliu1123/httpexchange/HttpExchangeRuntimeHintsRegistrar.java
@@ -5,15 +5,19 @@ import static org.springframework.aot.hint.MemberCategory.INVOKE_DECLARED_CONSTR
 import static org.springframework.aot.hint.MemberCategory.INVOKE_DECLARED_METHODS;
 
 import io.github.danielliu1123.httpexchange.shaded.requestfactory.EnhancedJdkClientHttpRequestFactory;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.aot.hint.ReflectionHints;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
-import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.http.client.AbstractClientHttpRequestFactoryWrapper;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.lang.Nullable;
 import org.springframework.util.ClassUtils;
@@ -38,29 +42,39 @@ class HttpExchangeRuntimeHintsRegistrar implements RuntimeHintsRegistrar {
     }
 
     private void registerForClientHttpRequestFactories(ReflectionHints reflection) {
-        Set<Class<?>> classes =
-                scanner
-                        .findCandidateComponents(
-                                ClientHttpRequestFactory.class.getPackage().getName())
-                        .stream()
-                        .map(BeanDefinition::getBeanClassName)
-                        .filter(StringUtils::hasText)
-                        .map(HttpExchangeRuntimeHintsRegistrar::forName)
-                        .filter(Objects::nonNull)
-                        .filter(ClientHttpRequestFactory.class::isAssignableFrom)
-                        .collect(Collectors.toSet());
-        classes.add(EnhancedJdkClientHttpRequestFactory.class);
-        classes.forEach(type ->
+        Set<Class<?>> factoryClasses = new HashSet<>(listFactory());
+        factoryClasses.add(EnhancedJdkClientHttpRequestFactory.class);
+        factoryClasses.forEach(type ->
                 reflection.registerType(type, INVOKE_DECLARED_METHODS, DECLARED_FIELDS, INVOKE_DECLARED_CONSTRUCTORS));
     }
 
+    Set<Class<?>> listFactory() {
+        return scanner
+                .findCandidateComponents(
+                        ClientHttpRequestFactory.class.getPackage().getName())
+                .stream()
+                .map(BeanDefinition::getBeanClassName)
+                .filter(StringUtils::hasText)
+                .map(HttpExchangeRuntimeHintsRegistrar::forName)
+                .filter(Objects::nonNull)
+                .filter(ClientHttpRequestFactory.class::isAssignableFrom)
+                .collect(Collectors.toSet());
+    }
+
     private static ClassPathScanningCandidateComponentProvider getScanner() {
-        return new ClassPathScanningCandidateComponentProvider(false) {
+        ClassPathScanningCandidateComponentProvider provider = new ClassPathScanningCandidateComponentProvider(false) {
             @Override
-            protected boolean isCandidateComponent(MetadataReader metadataReader) {
-                return true;
+            protected boolean isCandidateComponent(AnnotatedBeanDefinition beanDefinition) {
+                return true; // For including abstract classes
             }
         };
+        provider.addIncludeFilter((metadataReader, metadataReaderFactory) ->
+                Arrays.stream(metadataReader.getClassMetadata().getInterfaceNames())
+                                .anyMatch(intf -> Objects.equals(intf, ClientHttpRequestFactory.class.getName()))
+                        || Stream.ofNullable(metadataReader.getClassMetadata().getSuperClassName())
+                                .anyMatch(superClass -> Objects.equals(
+                                        superClass, AbstractClientHttpRequestFactoryWrapper.class.getName())));
+        return provider;
     }
 
     private static Class<?> forName(String className) {
