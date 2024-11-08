3918eafd7263e7b97c8821918d235e9bd41541bf
Freeman Liu <llw599502537@gmail.com>
2024-11-08 22:43:13 +0800

    refactor native-image example

--- examples/native-image/src/main/java/com/example/NativeImageApp.java
+++ examples/native-image/src/main/java/com/example/NativeImageApp.java
@@ -1,12 +1,10 @@
 package com.example;
 
 import io.github.danielliu1123.httpexchange.EnableExchangeClients;
-import io.github.danielliu1123.httpexchange.RequestConfigurator;
 import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.endpoint.event.RefreshEvent;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 
 @SpringBootApplication
@@ -14,26 +12,26 @@ import org.springframework.web.service.annotation.GetExchange;
 public class NativeImageApp {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(NativeImageApp.class, args);
-        PostApi postApi = ctx.getBean(PostApi.class);
+        var ctx = SpringApplication.run(NativeImageApp.class, args);
+
+        var postApi = ctx.getBean(PostApi.class);
 
         postApi.list().forEach(System.out::println);
-        postApi.withTimeout(2000).list().forEach(System.out::println);
 
-        System.setProperty("http-exchange.base-url", "http://jsonplaceholder.typicode.com");
+        System.out.println(postApi.get(1));
 
-        ctx.publishEvent(new RefreshEvent(ctx, null, null));
-
-        postApi.list().forEach(System.out::println);
-        postApi.withTimeout(2000).list().forEach(System.out::println);
-
-        System.clearProperty("http-exchange.base-url");
+        if (System.getenv("CI") != null) {
+            ctx.close();
+        }
     }
 
     public record Post(Integer id, String title) {}
 
-    public interface PostApi extends RequestConfigurator<PostApi> {
+    public interface PostApi {
         @GetExchange("/typicode/demo/posts")
         List<Post> list();
+
+        @GetExchange("/typicode/demo/posts/{id}")
+        Post get(@PathVariable("id") int id);
     }
 }
